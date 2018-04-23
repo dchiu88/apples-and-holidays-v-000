0a4a12dfd24b67cd37230b043f50e8e939bc05df
David Chiu <davidwchiu@gmail.com>
2018-04-23 23:33:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   +      puts "  #{capitalized_holiday}: #{supplies.join(', ')}"
   +    end
   +  end
-
+end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
